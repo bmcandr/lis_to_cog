e928095c3f3b599e2ef06158f41d27b0bc50aed5
Brendan McAndrew <19274445+bmcandr@users.noreply.github.com>
2022-06-12 23:08:52 -0400

    Add alternate conversion function

--- lis_to_cog.py
+++ lis_to_cog.py
@@ -1,8 +1,14 @@
 import os
+import numpy as np
 import rioxarray as rxr
 import xarray as xr
 
 from rasterio.crs import CRS
+from rasterio.io import MemoryFile
+from rio_cogeo.cogeo import cog_translate
+from rio_cogeo.profiles import cog_profiles
+from rasterio.warp import calculate_default_transform
+
 
 
 def _fix_coords(ds):
@@ -16,7 +22,7 @@ def _fix_coords(ds):
                 }) \
             .assign_coords(
                 lon=lons,
-                lat=lats
+                lat=lats[::-1]
                 )
 
     return ds
@@ -65,7 +71,7 @@ def _select_vars(ds, vars):
     return ds[[vars]]
 
 
-def lis_to_cog(inpath, vars=None, outdir=None):
+def to_cog(inpath, vars=None, outdir=None):
 
     ds = xr.open_dataset(inpath)
 
@@ -81,5 +87,69 @@ def lis_to_cog(inpath, vars=None, outdir=None):
     _write_cog(ds, inpath, outdir)
 
 
+def to_cog_v2(inpath, vars=None, outdir=None):
 
+    ds = xr.open_dataset(inpath)
+
+    if vars:
+        ds = _select_vars(ds, vars)
+
+    ds = _fix_coords(ds)
+
+    ds = _flatten_layers(ds)
+
+    ds_dtype = np.float32
+    nodata_value = ds.attrs["missing_value"]
+
+    x_variable, y_variable = "lon", "lat"
+
+    src_height, src_width = ds.dims[y_variable], ds.dims[x_variable]
+
+    lats, lons = ds[y_variable].values, ds[x_variable].values
+    xmin, xmax = np.min(lons), np.max(lons)
+    ymin, ymax = np.min(lats), np.max(lats)
+
+    src_crs = CRS.from_epsg(4326)
+    dst_crs = CRS.from_epsg(3857)
+
+    dst_transform, dst_width, dst_height = calculate_default_transform(
+        src_crs,
+        dst_crs,
+        src_width,
+        src_height,
+        left=xmin,
+        bottom=ymin,
+        right=xmax,
+        top=ymax,
+    )
+
+    output_profile = dict(
+        driver="GTiff",
+        dtype=ds_dtype,
+        count=len(ds),
+        transform=dst_transform,
+        crs=dst_crs,
+        height=src_height,
+        width=src_width,
+        nodata=nodata_value,
+        tiled=True,
+        compress="lzw",
+        blockxsize=256,
+        blockysize=256
+    )
+
+    filename = inpath.split('/')[-1][:-2] + "tif"
+    outpath = os.path.join(outdir, filename) if outdir else filename
+
+    with MemoryFile() as memfile:
+        with memfile.open(**output_profile) as mem:
+            data = ds.drop_dims(["time"]).to_array().values
+            mem.write(data)
+        cog_translate(
+            memfile,
+            outpath,
+            output_profile,
+            config=dict(GDAL_NUM_THREADS="ALL_CPUS", GDAL_TIFF_OVR_BLOCKSIZE="128")
+        )
+    return {"filename": outpath}
 
